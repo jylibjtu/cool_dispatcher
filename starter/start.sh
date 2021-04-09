b9a5d84eb8bbe28ea020587d2dd14688b9031d0a
junyi.li <junyi.li@qunar.com>
2021-04-09 14:54:28 +0800

    adjust start.sh

--- starter/start.sh
+++ starter/start.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 cd /home/q/cool_dispatcher/
 sudo chmod a+w -R .
-cd ./bin
-source /home/q/python36st/envs/transformer/bin/activate
+
+source /home/q/python36st/envs/cool_dispatcher/bin/activate
 c0=`ps aux | grep "message_dispatcher" | grep "$1" | grep -v "grep"| awk -F' ' '{print $2}'|wc -l`
 if [ $c0 -gt 0 ];
 then
  ps aux | grep "message_dispatcher" | grep "$1" | grep -v "grep"| awk -F' ' '{print $2}'|xargs -I {} sudo kill -9 {}
 fi
 
-sudo nohup python message_dispatcher.py -t $1 -e beta > /tmp/message_dispatcher.log 2>&1  &
-sudo nohup python start_web_service.py -c restart -t $1 -e beta > /tmp/web_service_start.log 2>&1 &
+sudo nohup python core/message_dispatcher.py -t $1 -e beta > /tmp/message_dispatcher.log 2>&1  &
+sudo nohup python starter/start_web_service.py -c restart -t $1 -e beta > /tmp/web_service_start.log 2>&1 &
